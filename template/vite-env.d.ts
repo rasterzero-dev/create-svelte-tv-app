bb20ae843526d637a80d6bc31488242524f85700
SpongeBed81 <cmodz7969@gmail.com>
2026-07-01 13:43:05 +0300

    feat: add virtual package for fonts

---
+++ template/vite-env.d.ts
@@ -0,0 +1,5 @@
+declare module 'virtual:svelte-tv/generated-fonts' {
+    export const generatedFonts: import('svelte-tv/fonts').GeneratedFonts;
+    export const generatedFontManifest: import('svelte-tv/fonts').GeneratedFontManifest;
+  }
+  
